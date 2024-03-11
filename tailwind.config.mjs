5342c299c411a914f13c764a2d3450183091dd54
Daniel Cordero <dn.lambb@gmail.com>
2024-03-11 17:05:23 -0300

    feat: add jetbrains mono font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetbrains: ['JetBrains Mono Variable', 'monospace', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
