05936f8ba917672cd1f6f892d5ba8aef473563f2
Daniel Cordero <dn.lambb@gmail.com>
2024-04-20 20:04:45 -0400

    refactor: add styles for hover of button in gallery

--- src/components/ButtonGallery.tsx
+++ src/components/ButtonGallery.tsx
@@ -1,7 +1,7 @@
 export const ButtonGallery = ({ children, active, ...props }: any) => {
   return (
     <button
-      class={`h-12 px-8 py-2 -mb-px text-sm text-center bg-transparent border-b whitespace-nowrap ${
+      class={`h-12 px-8 py-2 -mb-px text-sm text-center bg-transparent border-b whitespace-nowrap hover:text-yellow-200 ${
         active
           ? 'text-yellow-200 border-yellow-200'
           : 'text-zinc-200 border-zinc-800'
