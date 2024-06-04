ac076ec72cc881e1f3294e39ca9e51c59018c402
Daniel Cordero <daniel@corderodev.com>
2024-06-04 03:13:39 -0400

    fix: add sqoosh service in astro.config.mjs
    change image processing service to avoid build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  image: {
+    service: squooshImageService(),
+  },
 });
