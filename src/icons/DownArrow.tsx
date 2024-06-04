da9ae1a1ff49d1f5fcde75303b7da12149dcdd7e
Daniel Cordero <dn.lambb@gmail.com>
2024-06-04 01:45:21 -0400

    feat: change svg of DownArrowIcon component

--- src/icons/DownArrow.tsx
+++ src/icons/DownArrow.tsx
@@ -2,20 +2,16 @@ export const DownArrowIcon = ({ className, ...props }: any) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
+      width='128'
+      height='128'
       viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      className={`icon icon-tabler icons-tabler-outline icon-tabler-select ${className}`}
+      className={className}
       {...props}
     >
-      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-      <path d='M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z'></path>
-      <path d='M9 11l3 3l3 -3'></path>
+      <path
+        fill='currentColor'
+        d='M12 21.9q-.2 0-.375-.075T11.3 21.6l-5.6-5.575q-.275-.275-.275-.7T5.7 14.6q.3-.3.713-.3t.712.3L11 18.5v-6.175q0-.425.288-.712t.712-.288t.713.288t.287.712V18.5l3.9-3.9q.275-.275.688-.275t.712.3q.275.275.275.7t-.275.7L12.7 21.6q-.15.15-.325.225T12 21.9m0-12.575q-.425 0-.712-.287T11 8.325v-1q0-.425.288-.712T12 6.325t.713.288t.287.712v1q0 .425-.288.713T12 9.325m0-5q-.425 0-.712-.287T11 3.325t.288-.712t.712-.288t.713.288t.287.712t-.288.713t-.712.287'
+      />
     </svg>
   )
 }
