3426cc23fe696201e469bcc38b3bc4b8f63e800c
Daniel Cordero <daniel@corderodev.com>
2024-07-15 21:57:22 -0400

    fix: resolve typo in properties of icons components

--- src/icons/Phone.tsx
+++ src/icons/Phone.tsx
@@ -7,9 +7,9 @@ export const Phone = ({ className, ...props }: any) => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       class={`icon icon-tabler icons-tabler-outline icon-tabler-phone ${className}`}
       {...props}
     >
--- src/icons/World.tsx
+++ src/icons/World.tsx
@@ -7,9 +7,9 @@ export const WorldIcon = ({ className, ...props }: any) => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       class={`icon icon-tabler icons-tabler-outline icon-tabler-world ${className}`}
       {...props}
     >
