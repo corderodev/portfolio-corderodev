3f50a965c27dd0948d98a038380d1dd1c2222944
Daniel Cordero <daniel@corderodev.com>
2024-08-12 15:05:06 -0400

    feat: update the certificate section with new cards and carousel

---
+++ src/icons/certificates/Platzi.tsx
@@ -0,0 +1,17 @@
+export const Platzi = ({ className, ...props }: any) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='128'
+      height='128'
+      viewBox='0 0 24 24'
+      className={className}
+      {...props}
+    >
+      <path
+        fill='currentColor'
+        d='M10.64 1.127L2.485 9.282a3.84 3.84 0 0 0 0 5.436l8.155 8.155a3.84 3.84 0 0 0 5.436 0l2.72-2.718l-2.72-2.718l-2.718 2.718L5.203 12l8.155-8.155l5.437 5.437l-5.437 5.436l2.718 2.72L21.513 12a3.84 3.84 0 0 0 0-5.437l-5.448-5.436a3.83 3.83 0 0 0-5.425 0'
+      />
+    </svg>
+  )
+}
